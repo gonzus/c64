ed0a6bae8e9465dae8f2228a96c541ef0f7361cf
Gonzalo Diethelm <gonzalo.diethelm@gmail.com>
2021-01-21 16:52:34 +0100

    Addressing mode Zero Page, refactored tests

--- src/cpu.zig
+++ src/cpu.zig
@@ -23,11 +23,13 @@ pub const CPU = struct {
 
     const AddressingMode = enum {
         Immediate,
+        ZeroPage,
         Absolute,
     };
 
     const OP = enum(Type.Byte) {
         LDA_IMM = 0xA9,
+        LDA_ZP = 0xA5,
         LDA_ABS = 0xAD,
 
         NOP = 0xEA,
@@ -69,6 +71,11 @@ pub const CPU = struct {
                     self.A = value;
                     self.setNZ(value);
                 },
+                OP.LDA_ZP => {
+                    const value = self.fetch(.ZeroPage);
+                    self.A = value;
+                    self.setNZ(value);
+                },
                 OP.LDA_ABS => {
                     const value = self.fetch(.Absolute);
                     self.A = value;
@@ -85,6 +92,11 @@ pub const CPU = struct {
     fn fetch(self: *CPU, mode: AddressingMode) Type.Byte {
         const fetched = switch (mode) {
             .Immediate => self.memory.data[self.PC],
+            .ZeroPage => blk: {
+                const address = @as(Type.Word, self.memory.data[self.PC + 0]);
+                self.tick();
+                break :blk self.memory.data[address];
+            },
             .Absolute => blk: {
                 const lo = @as(Type.Word, self.memory.data[self.PC + 0]) << 0;
                 const hi = @as(Type.Word, self.memory.data[self.PC + 1]) << 8;
@@ -109,66 +121,65 @@ pub const CPU = struct {
     }
 };
 
+const TEST_ADDRESS = 0x4433;
+
 test "create CPU" {
     var cpu = CPU.init();
     testing.expect(cpu.PC == CPU.INITIAL_ADDRESS);
+    cpu.reset(TEST_ADDRESS);
+    testing.expect(cpu.PC == TEST_ADDRESS);
+}
+
+fn test_lda(cpu: *CPU, address: Type.Word, ticks: u32) void {
+    const Data = struct {
+        v: Type.Byte,
+        N: Type.Bit,
+        Z: Type.Bit,
+    };
+    const data = [_]Data{
+        .{ .v = 0x11, .N = 0, .Z = 0 },
+        .{ .v = 0xF0, .N = 1, .Z = 0 },
+        .{ .v = 0x00, .N = 0, .Z = 1 },
+    };
+    for (data) |d| {
+        cpu.PC = TEST_ADDRESS;
+        cpu.memory.data[address] = d.v;
+        const used = cpu.run(ticks);
+        testing.expect(used == ticks);
+        testing.expect(cpu.A == d.v);
+        testing.expect(cpu.P.bits.N == d.N);
+        testing.expect(cpu.P.bits.Z == d.Z);
+    }
 }
 
 test "run LDA_IMM" {
     var cpu = CPU.init();
+    cpu.reset(TEST_ADDRESS);
+    cpu.memory.data[TEST_ADDRESS + 0x00] = 0xA9;
+    test_lda(&cpu, TEST_ADDRESS + 0x01, 2);
+}
 
-    const Data = struct {
-        v: Type.Byte,
-        N: Type.Bit,
-        Z: Type.Bit,
-    };
-    const data = [_]Data{
-        .{ .v = 0x11, .N = 0, .Z = 0 },
-        .{ .v = 0xF0, .N = 1, .Z = 0 },
-        .{ .v = 0x00, .N = 0, .Z = 1 },
-    };
-    for (data) |d| {
-        cpu.reset(CPU.INITIAL_ADDRESS);
-        cpu.memory.data[CPU.INITIAL_ADDRESS + 0x00] = 0xA9;
-        cpu.memory.data[CPU.INITIAL_ADDRESS + 0x01] = d.v;
-        const used = cpu.run(2);
-        testing.expect(used == 2);
-        testing.expect(cpu.A == d.v);
-        testing.expect(cpu.P.bits.N == d.N);
-        testing.expect(cpu.P.bits.Z == d.Z);
-    }
+test "run LDA_ZP" {
+    var cpu = CPU.init();
+    cpu.reset(TEST_ADDRESS);
+    cpu.memory.data[TEST_ADDRESS + 0x00] = 0xA5;
+    cpu.memory.data[TEST_ADDRESS + 0x01] = 0x11;
+    test_lda(&cpu, 0x0011, 3);
 }
 
 test "run LDA_ABS" {
     var cpu = CPU.init();
-
-    const Data = struct {
-        v: Type.Byte,
-        N: Type.Bit,
-        Z: Type.Bit,
-    };
-    const data = [_]Data{
-        .{ .v = 0x11, .N = 0, .Z = 0 },
-        .{ .v = 0xF0, .N = 1, .Z = 0 },
-        .{ .v = 0x00, .N = 0, .Z = 1 },
-    };
-    for (data) |d| {
-        cpu.reset(CPU.INITIAL_ADDRESS);
-        cpu.memory.data[CPU.INITIAL_ADDRESS + 0x00] = 0xAD;
-        cpu.memory.data[CPU.INITIAL_ADDRESS + 0x01] = 0x11;
-        cpu.memory.data[CPU.INITIAL_ADDRESS + 0x02] = 0x83;
-        cpu.memory.data[0x8311] = d.v;
-        const used = cpu.run(4);
-        testing.expect(used == 4);
-        testing.expect(cpu.A == d.v);
-        testing.expect(cpu.P.bits.N == d.N);
-        testing.expect(cpu.P.bits.Z == d.Z);
-    }
+    cpu.reset(TEST_ADDRESS);
+    cpu.memory.data[TEST_ADDRESS + 0x00] = 0xAD;
+    cpu.memory.data[TEST_ADDRESS + 0x01] = 0x11;
+    cpu.memory.data[TEST_ADDRESS + 0x02] = 0x83;
+    test_lda(&cpu, 0x8311, 4);
 }
 
 test "run NOP" {
     var cpu = CPU.init();
-    cpu.memory.data[CPU.INITIAL_ADDRESS + 0x00] = 0xEA;
+    cpu.reset(TEST_ADDRESS);
+    cpu.memory.data[TEST_ADDRESS + 0x00] = 0xEA;
     const used = cpu.run(2);
     testing.expect(used == 2);
 }
