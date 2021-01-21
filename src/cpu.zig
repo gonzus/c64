db50c1e78891d8bd1136ab3adc33a4e3189ba259
Gonzalo Diethelm <gonzalo.diethelm@gmail.com>
2021-01-21 16:32:08 +0100

    Addressing mode Absolute, opcode LDA_ABS

--- src/cpu.zig
+++ src/cpu.zig
@@ -21,8 +21,14 @@ pub const CPU = struct {
 
     ticks: u32, // Cycle counter
 
+    const AddressingMode = enum {
+        Immediate,
+        Absolute,
+    };
+
     const OP = enum(Type.Byte) {
         LDA_IMM = 0xA9,
+        LDA_ABS = 0xAD,
 
         NOP = 0xEA,
     };
@@ -56,10 +62,15 @@ pub const CPU = struct {
     pub fn run(self: *CPU, limit: u32) u32 {
         const start = self.ticks;
         while ((self.ticks - start) < limit) {
-            const op = @intToEnum(OP, self.fetch());
+            const op = @intToEnum(OP, self.fetch(.Immediate));
             switch (op) {
                 OP.LDA_IMM => {
-                    const value = self.fetch();
+                    const value = self.fetch(.Immediate);
+                    self.A = value;
+                    self.setNZ(value);
+                },
+                OP.LDA_ABS => {
+                    const value = self.fetch(.Absolute);
                     self.A = value;
                     self.setNZ(value);
                 },
@@ -71,8 +82,18 @@ pub const CPU = struct {
         return self.ticks - start;
     }
 
-    fn fetch(self: *CPU) Type.Byte {
-        const fetched = self.memory.data[self.PC];
+    fn fetch(self: *CPU, mode: AddressingMode) Type.Byte {
+        const fetched = switch (mode) {
+            .Immediate => self.memory.data[self.PC],
+            .Absolute => blk: {
+                const lo = @as(Type.Word, self.memory.data[self.PC + 0]) << 0;
+                const hi = @as(Type.Word, self.memory.data[self.PC + 1]) << 8;
+                const address = hi | lo;
+                self.tick();
+                self.tick();
+                break :blk self.memory.data[address];
+            },
+        };
         self.tick();
         self.PC += 1;
         return fetched;
@@ -112,6 +133,33 @@ test "run LDA_IMM" {
         cpu.memory.data[CPU.INITIAL_ADDRESS + 0x01] = d.v;
         const used = cpu.run(2);
         testing.expect(used == 2);
+        testing.expect(cpu.A == d.v);
+        testing.expect(cpu.P.bits.N == d.N);
+        testing.expect(cpu.P.bits.Z == d.Z);
+    }
+}
+
+test "run LDA_ABS" {
+    var cpu = CPU.init();
+
+    const Data = struct {
+        v: Type.Byte,
+        N: Type.Bit,
+        Z: Type.Bit,
+    };
+    const data = [_]Data{
+        .{ .v = 0x11, .N = 0, .Z = 0 },
+        .{ .v = 0xF0, .N = 1, .Z = 0 },
+        .{ .v = 0x00, .N = 0, .Z = 1 },
+    };
+    for (data) |d| {
+        cpu.reset(CPU.INITIAL_ADDRESS);
+        cpu.memory.data[CPU.INITIAL_ADDRESS + 0x00] = 0xAD;
+        cpu.memory.data[CPU.INITIAL_ADDRESS + 0x01] = 0x11;
+        cpu.memory.data[CPU.INITIAL_ADDRESS + 0x02] = 0x83;
+        cpu.memory.data[0x8311] = d.v;
+        const used = cpu.run(4);
+        testing.expect(used == 4);
         testing.expect(cpu.A == d.v);
         testing.expect(cpu.P.bits.N == d.N);
         testing.expect(cpu.P.bits.Z == d.Z);
